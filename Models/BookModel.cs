b41ad59af127e999cd993acb508f89cfbaaa7697
Manoj Reddy <luckymanoj1116@gmail.com>
2022-08-20 12:47:01 -0400

    final

---
+++ Models/BookModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookTrackingApp.Models
+{
+    public class BookModel
+    {
+        [Key]
+        public int BookId { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR(50)")]
+        [MaxLength(13,ErrorMessage ="Minimum Length is 13")]
+        [MinLength(10, ErrorMessage = "Minimum Length Length is 10")]
+        public string ISBN { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR(250)")]
+        [MaxLength(250, ErrorMessage = "Maximum Length is 250")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        [ForeignKey("CategoryModel")]
+        public int Category { get; set; }
+        public CategoryModel CategoryModel { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR(250)")]
+        [MaxLength(250, ErrorMessage = "Maximum Length is 250")]
+        public string Author { get; set; }
+    }
+}
---
+++ Models/CategoryTypeModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookTrackingApp.Models
+{
+    public class CategoryTypeModel
+    {
+        [Key]
+        public int CategoryTypeId { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR(250)")]
+        [MaxLength(250, ErrorMessage = "Maximum Length is 250")]
+        public string Type { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR(250)")]
+        [MaxLength(250, ErrorMessage = "Maximum Length is 250")]
+        public string Name { get; set; }
+
+
+        //RelationShip
+        public List<CategoryModel> CategoryList { get; set; }
+    }
+}
